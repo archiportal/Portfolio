4a003f3a5302a727537d4f847e424465e11d47f6
Archisman Roy <archismanroy@Archismans-MacBook-Air.local>
2022-12-21 14:47:18 +0530

    email added

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -19,15 +19,20 @@ function Intro() {
         </div>
         <p className='text-2xl py-2 font-burtons text-gray-600'></p>
     </div>
+    <div className='flex flex-col gap-4'>
       <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
         <a href='https://www.linkedin.com/in/archismanroy007/'>
-        <AiFillLinkedin className='cursor-pointer text-teal-700'/>
+        <AiFillLinkedin className='cursor-pointer text-teal-700 hover:text-teal-500'/>
         </a>
         <a href='https://github.com/archiportal'>
-        <AiFillGithub className='cursor-pointer text-teal-700'/>
+        <AiFillGithub className='cursor-pointer text-teal-700 hover:text-teal-500'/>
         </a>
-        <AiFillInstagram className='cursor-pointer text-teal-700'/>
+        <AiFillInstagram className='cursor-pointer text-teal-700 hover:text-teal-500'/>
       </div>
+      <div className='flex justify-center mt-4'>
+        <p className='bg-teal-700 text-slate-100 md:text-2xl text-xl px-4 py-4 rounded-xl hover:bg-teal-600'>e-mail : royarchisman17@gmail.com</p>
+      </div>
+    </div>
       <img data-aos='fade-up' src={Bitmoji} alt="Personal Logo" className='mx-auto rounded-full h-1/2 w-1/2 md:h-2/5 md:w-2/5 lg:h-1/3 lg:w-1/3 m-20 shadow-md shadow-slate-400'/>
     </div>
   )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-        <div className=' h-full w-full lg:w-1/2 flex flex-col justify-center gap-3 md:p-7 py-7 text-center rounded-lg bg-green-100 shadow-md shadow-slate-400'>
+        <div className=' h-full w-full lg:w-1/2 flex flex-col justify-center gap-3 md:p-7 py-7 text-center rounded-lg bg-gradient-to-b from-green-100 to-green-50 shadow-md shadow-slate-400'>
             <h3 className='text-5xl font-burtons text-teal-700 p-2'>Skills</h3>
             <div className='flex flex-col p-2 justify-center'>
             <span className='text-3xl font-burtons text-teal-700 mb-2'>Java :</span>
